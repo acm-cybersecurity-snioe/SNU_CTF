efafb8fbfed2fb9c7d1a0e841fbcf5f3083681f7
Ruchir .U. Kalla <164368036+WifiXSmasher@users.noreply.github.com>
2025-08-02 23:04:31 +0530

    Update settings.py
    hmm

--- portfolio_django/settings.py
+++ portfolio_django/settings.py
@@ -27,13 +27,13 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 # DEBUG = True#turen off for vercel deployment
 # DEBUG=False
-DEBUG=config('DEBUG')
+# DEBUG=config('DEBUG')
+DEBUG = config('DEBUG', default=False, cast=bool)
 
 ALLOWED_HOSTS = [
     'localhost',
     '127.0.0.1',
-    '.vercel.app',
-    '*',     
+    '.vercel.app', 
 ]
 
 
